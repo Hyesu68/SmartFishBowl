a4069e96b6155fba6e712606be7399afa6ef3f7c
hyesu <hyesulee0608@gmail.com>
2019-08-03 14:54:33 +0900

    Android

---
+++ SmartFishBowl2/app/src/main/java/com/example/hs/smartfishbowl/FirebaseMessagingIDService.java
@@ -0,0 +1,53 @@
+package com.example.hs.smartfishbowl;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+//메시지를 받기위한 ID식별자를 FCM서버에 등록
+public class FirebaseMessagingIDService extends FirebaseMessagingService {
+    private static final String TAG = "Service";
+
+    @Override
+    public void onNewToken(String s){
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        Log.d(TAG,"From: "+remoteMessage);
+        Log.d(TAG,remoteMessage.getData().toString());
+        sendNotification(remoteMessage);
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        //NotificationChannel mChannel = new NotificationChannel("and","and", NotificationManager.IMPORTANCE_DEFAULT);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification.Builder notificationBuilder = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setContentText(remoteMessage.getData().toString())
+                .setSound(defaultSoundUri)
+                .setVibrate(new long[]{1000, 1000})
+                .setLights(Color.BLUE, 1,1)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
